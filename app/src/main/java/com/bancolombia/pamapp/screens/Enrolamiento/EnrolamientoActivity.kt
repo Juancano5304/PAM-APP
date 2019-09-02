b9167ce7626f83d3b92d23e9cde788a5655e7941
carlos.ardila <carlos.ardila@pragma.com.co>
2019-09-02 18:16:44 -0500

    Log in enrolamiento

--- app/src/main/java/com/bancolombia/pamapp/screens/Enrolamiento/EnrolamientoActivity.kt
+++ app/src/main/java/com/bancolombia/pamapp/screens/Enrolamiento/EnrolamientoActivity.kt
@@ -2,12 +2,37 @@ package com.bancolombia.pamapp.screens.Enrolamiento
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.bancolombia.pamapp.R
+import kotlinx.android.synthetic.main.activity_enrolamiento.*
 
 class EnrolamientoActivity : AppCompatActivity() {
+    private lateinit var enrolVM:EnrolamientoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enrolamiento)
+        enrolVM=ViewModelProviders.of(this).get(EnrolamientoViewModel::class.java)
+        buttonIngresar.setOnClickListener {
+            enrolVM.login(editTextUser.text.toString(),editTextPassword.text.toString())
+        }
+        adbObserver()
     }
+
+    private fun adbObserver(){
+        val observer= Observer<String>{
+                valor->
+            if (valor!=null){
+                when(valor){
+                    "UserEmpty"->editTextUser.error="Usuario invalido"
+                    "PasswordEmpty"->editTextPassword.error="Contraseña invalida"
+                    "Ready"->editTextPassword.error="Ha accedido correctamente"
+                }
+            }
+        }
+        enrolVM.devolucion.observe(this,observer)
+    }
+
 }
--- app/src/main/java/com/bancolombia/pamapp/screens/Enrolamiento/EnrolamientoViewModel.kt
+++ app/src/main/java/com/bancolombia/pamapp/screens/Enrolamiento/EnrolamientoViewModel.kt
@@ -1,8 +1,37 @@
 package com.bancolombia.pamapp.screens.Enrolamiento
 
 import android.app.Application
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import android.telephony.TelephonyManager
 
-class EnrolamientoViewModel(applicaton:Application):ViewModel(){
 
+
+class EnrolamientoViewModel(aplication:Application): AndroidViewModel(aplication){
+    val state_login=MutableLiveData<String>()
+    val devolucion:LiveData<String>
+        get() =state_login
+    lateinit var telefono:TelephonyManager
+//    fun validateText(textEvaluate:String):Boolean{
+//        var respuesta=true
+//        if(textEvaluate.isEmpty()){
+//            respuesta=false
+//        }
+//       return respuesta
+//    }
+
+    fun login(usuario:String,password:String){
+        if(usuario.isNotEmpty()){
+            if(password.isNotEmpty()){
+                state_login.value="Ready"
+                //ENVIAR PETICION HTTP
+            }else{
+                state_login.value="PasswordEmpty"
+            }
+        }else{
+            state_login.value="UserEmpty"
+        }
+
+    }
 }
